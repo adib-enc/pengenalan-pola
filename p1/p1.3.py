2a792a7c579f84712019cfd2aa0c4c139db3773f
adib-enc <adib35785@gmail.com>
2021-08-25 09:59:13 +0700

    g colab notebook

--- p1/p1.3.py
+++ p1/p1.3.py
@@ -6,19 +6,30 @@
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+import time
+
+def readAndShowImg(fname):
+    img = cv2.imread(fname)
+    cv2.imshow("img", img)
+    return img
+
 
 print("read images using opencv")
 five = cv2.imread("p-5.png")
 print(five.shape)
 print(five.size)
+
+# cv2.imshow("ims", five)
 plt.imshow(five)
 cv2.waitKey(0)
 
-im = cv2.imread("ipb.png")
-plt.imshow(im)
+img = cv2.imread("ipb.png")
+# cv2.imshow("img", img)
+plt.imshow(img)
 
-im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-plt.imshow(im)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# cv2.imshow("img", img)
+plt.imshow(img)
 
 # mengambil nilai matriksnya
 # acces pixel of images per postion 
